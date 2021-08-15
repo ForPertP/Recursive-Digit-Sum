b9a8e0e5cadb84c0a70d1590378f110c4c47ed4a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-08-16 05:33:41 +0900

    Update Recursive-Digit-Sum.cpp

--- Recursive-Digit-Sum.cpp
+++ Recursive-Digit-Sum.cpp
@@ -1,1 +1,21 @@
+int superDigit(string n, int k)
+{
+    long long int sum = 0;
+    
+    for (int i = 0 ; i < n.size() ; ++i)
+    {
+        sum = sum + k * (n[i] - 48);
+    }
+     
+    string p = to_string(sum);    
+
+    if (p.length() == 1)
+    {
+        return(p[0]-48); 
+    }
+    else
+    {
+        return superDigit(p,1);
+    }
+}
 
