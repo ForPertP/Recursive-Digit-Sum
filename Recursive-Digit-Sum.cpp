aead9073cff075e98323e4f1aa2e628964ecbfa4
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-09-27 02:35:17 +0900

    Update Recursive-Digit-Sum.cpp

--- Recursive-Digit-Sum.cpp
+++ Recursive-Digit-Sum.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'superDigit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING n
+ *  2. INTEGER k
+ */
+
 int superDigit(string n, int k)
 {
     long long int sum = 0;
