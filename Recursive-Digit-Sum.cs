53b100220384507bc77c77f4aa61ceb52dd8797c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-10-07 04:59:32 +0900

    Update Recursive-Digit-Sum.cs

--- Recursive-Digit-Sum.cs
+++ Recursive-Digit-Sum.cs
@@ -1,1 +1,34 @@
-Recursive-Digit-Sum
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(string n, int k)
+    {
+        long sum = 0;
+        return (int)sum;
+    }
+}
+
+
