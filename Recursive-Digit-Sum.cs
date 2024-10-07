496013a28595b88329e49f231d51fd10749ca405
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-10-08 02:56:33 +0900

    Update Recursive-Digit-Sum.cs

--- Recursive-Digit-Sum.cs
+++ Recursive-Digit-Sum.cs
@@ -32,3 +32,24 @@ class Result
 }
 
 
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        string n = firstMultipleInput[0];
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
