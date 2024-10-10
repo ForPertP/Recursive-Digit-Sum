4080165de2196905ede0a96f1e1b7a9dd7128b58
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-10-11 06:09:45 +0900

    Update Recursive-Digit-Sum.cpp

--- Recursive-Digit-Sum.cpp
+++ Recursive-Digit-Sum.cpp
@@ -1,108 +1,79 @@
-#include <bits/stdc++.h>
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'superDigit' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. STRING n
- *  2. INTEGER k
- */
-
-int getSuperDigit(long long sum)
+class Result
 {
-    if (sum < 10)
-    {
-        return sum;
-    }
 
-    long long newSum = 0;
-    while (sum > 0)
-    {
-        newSum += sum % 10;
-        sum /= 10;
-    }
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
 
-    return getSuperDigit(newSum);
+    private static int getSuperDigit(long num)
+    {
+        if (num < 10)
+        {
+            return (int)num;
+        }
+
+        long newSum = 0;
+        while (num > 0)
+        {
+            newSum += num % 10;
+            num /= 10;
+        }
+
+        return getSuperDigit(newSum);
+    }
+    
+    public static int superDigit(string n, int k)
+    {
+        long sum = 0;
+        for (int i = 0; i < n.Length; i++)
+        {
+            sum += n[i] - '0';
+        }
+
+        sum *= k;
+
+        return getSuperDigit(sum);
+    }
+    
 }
 
-int superDigit(string n, int k)
+
+class Solution
 {
-    long long sum = 0;
-    for (int i = 0; i < n.size(); i++)
+    public static void Main(string[] args)
     {
-        sum += n[i] - '0';
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        string n = firstMultipleInput[0];
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
     }
-
-    sum *= k;
-
-    return getSuperDigit(sum);
-}
-
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
-
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
-
-    string n = first_multiple_input[0];
-
-    int k = stoi(first_multiple_input[1]);
-
-    int result = superDigit(n, k);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
