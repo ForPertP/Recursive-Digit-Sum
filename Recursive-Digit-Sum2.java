96f431c11497e64a36537c13adfaff723952aff5
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-10-18 03:03:07 +0900

    Update Recursive-Digit-Sum2.java

--- Recursive-Digit-Sum2.java
+++ Recursive-Digit-Sum2.java
@@ -21,6 +21,26 @@ class Result {
      *  2. INTEGER k
      */
 
-
 }
 
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
